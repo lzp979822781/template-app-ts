baa52ae65fe8299f7df72e0123656a54d86a63a1
liuzhipeng26 <liuzhipeng26@jd.com>
2020-04-10 17:09:51 +0800

    revise build bundle command to compatiable delete bundle

--- scripts/build-rn-jsbundle.js
+++ scripts/build-rn-jsbundle.js
@@ -1,6 +1,8 @@
 /* eslint-disable import/no-commonjs */
 const fs = require('fs');
 const { exec } = require("child_process");
+const argv = process.argv;
+const isClean = argv.includes("clean");
 
 const buildCommand = "node ./node_modules/react-native/local-cli/cli.js bundle --entry-file ./rn_temp/index.js --bundle-output ./bundle/index.bundle --assets-dest ./bundle --platform ios --dev false --verbose";
 const iosBuild = "node ./node_modules/react-native/local-cli/cli.js bundle --entry-file ./rn_temp/index.js --bundle-output ./bundle/index.bundle --assets-dest ./bundle --platform ios --dev false --verbose";
@@ -46,7 +48,16 @@ function execBuild() {
 }
 
 fs.exists('bundle', function(exists) {
+    if(isClean) {
+        clean(exists);
+        return;
+    } 
+    
+    build(exists)
+    
+})
 
+function build(exists) {
     if(exists) {
         // 删除文件夹及其子文件夹内容
         deleteDir()
@@ -54,5 +65,11 @@ fs.exists('bundle', function(exists) {
         // 创建文件夹并执行命令
         mkDir();
     }
-})
+}
+
+function clean(exists) {
+    if(exists) { 
+        deleteDir() 
+    }
+}
 
