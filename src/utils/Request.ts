d9fa06d3337bb95808de7c32feee190f4951e5b4
liuzhipeng26 <liuzhipeng26@jd.com>
2020-04-27 17:25:19 +0800

    optimize Request code

--- src/utils/Request.ts
+++ src/utils/Request.ts
@@ -1,5 +1,6 @@
 import hmacSHA512 from "crypto-js/hmac-sha512";
-import Taro, { Component, Config } from "@tarojs/taro";
+import Taro from "@tarojs/taro";
+import pick from 'lodash/pick';
 
 // const host = 'https://yaoser.jd.com/';
 // const JDYJCMainURL = 'https://api.m.jd.com/api'
@@ -24,8 +25,8 @@ export default class Request {
         return this.getRequest(url, param);
     }
 
-    static post(url, param, callback) {
-        this.postRequest(url, param, callback);
+    static post(url, param) {
+        return this.postRequest(url, param);
     }
 
     static getRequest(url, param) {
@@ -47,7 +48,7 @@ export default class Request {
         });
     }
 
-    static postRequest(url, param, callback) {
+    static postRequest(url, param) {
         let contentType = CONTENT_TYPE.FORM_WWW;
         if ("yjc_address_subAddress" === url) {
             contentType = CONTENT_TYPE.FORM_WWW;
@@ -72,28 +73,25 @@ export default class Request {
             method: "POST",
             data: requestPara,
             header: config.headers,
-            success: function(res) {
-                if (callback) {
-                    callback(resp.data, null);
-                }
-                callback = null;
-            }
         });
     }
 
+    /**
+     * 
+     * @param {string} sign 签名字段
+     * 
+     */
     static dealPostBody(url, param, contentType) {
         //0、参数整理为字典类型
-        let objc = this.initParam(url, param, contentType);
+        const objc = this.initParam(url, param, contentType);
         //1、参数整理（公参、业务参数）
-        let newParams = this.dealWithParam(objc, param);
+        const newParams = this.dealWithParam(objc, param);
         //2、ASCII 顺序拼接签名参数
-        let paraAppendString = this.appendSingnParaString(newParams);
+        const paraAppendString = this.appendSingnParaString(newParams);
         //3、参数签名
-        let hmactring = hmacSha256(paraAppendString, secretKey);
-        //4、参数中加入签名字段
-        newParams["sign"] = hmactring;
+        const hmactring = hmacSHA512(paraAppendString, secretKey);
 
-        return newParams;
+        return Object.assign({}, newParams, { sign: hmactring });
     }
 
     /**
@@ -117,27 +115,23 @@ export default class Request {
         return urlString; //返回URL
     }
 
-    static initParam(functionId, para, contentType) {
-        var params = this.params; //获取url中"?"符后的字串
+    static genBody = (para, contentType) => {
+        const paraNew = {
+            data: para,
+            contentType
+        };
+        return Object.keys(para).length ? { body: JSON.stringify(paraNew) } : {};
+    }
+
+    static initParam(functionId: string, para, contentType: string) {
         //存放所有的公共参数
-        var objc = new Object();
-        // var str = params.substr(1); //从第一个字符开始截取
-        // strs = str.split("&"); //以&符号分离
-        // for (var i = 0; i < strs.length; i++) {
-        //     objc[strs[i].split("=")[0]] = strs[i].split("=")[1];
-        // }
-        objc["functionId"] = functionId;
-        objc["t"] = Date.parse(Date());
-        if (Object.keys(para).length) {
-            let paraNew = {
-                data: para,
-                contentType: contentType
-            };
-            objc["body"] = JSON.stringify(paraNew);
-        }
-        objc["contentType"] = contentType;
-        objc["appid"] = "yjc_app";
-        return objc;
+        var objc = {
+            functionId,
+            t: Date.parse(Date()),
+            contentType,
+            appid: "yjc_app",
+        };
+        return Object.assign({}, objc, this.genBody(para, contentType))
     }
 
     /**
@@ -146,14 +140,7 @@ export default class Request {
      */
     static dealWithParam(objc, para) {
         //1、参数整理（公参、业务参数）
-        let newParams = {};
-        newParams["functionId"] = objc["functionId"];
-        newParams["t"] = objc["t"];
-        if (Object.keys(para).length) {
-            newParams["body"] = objc["body"];
-        }
-        // newParams["contentType"] = objc["contentType"];
-        newParams["appid"] = objc["appid"];
+        let newParams = Object.assign({}, pick(objc, ["functionId", "t", "appid", "body"]));
         // newParams["uuid"] = objc["uuid"];
         // newParams["client"] = objc["clientType"];
         // newParams["clientVersion"] = objc["clientVersion"];
@@ -171,25 +158,17 @@ export default class Request {
      */
     static appendSingnParaString(newParams) {
         let paraKeyArray = this.mapKeySortASC(newParams);
-        let hmacSha256String = "";
-        for (let index = 0; index < paraKeyArray.length; index++) {
-            const element = paraKeyArray[index];
-            if (index == 0) {
-                hmacSha256String += newParams[element];
-            } else {
-                hmacSha256String += "&" + newParams[element];
-            }
-        }
-        return hmacSha256String;
+        return paraKeyArray.reduce((total, key) => `${total}&${newParams[key]}`, '').substring(1);
     }
 
     /**
      *  字典的 key 按照ASCii 排序
-     * @param {字典} map
+     * @param {Object} objc
+     * @returns {Array} newkeyArray 返回objc字段数组
      */
     static mapKeySortASC(objc) {
         var newkeyArray = Object.keys(objc).sort();
-        return newkeyArray; //返回排好序的新对象
+        return newkeyArray; //返回排好序的新数组
     }
 
     /**
@@ -198,21 +177,7 @@ export default class Request {
      * @return { 返回 字符串 } bodyString
      */
     static convertDicToKeyValueString(postBody) {
-        //body字符串变量
-        let bodyString = "";
-        //拼接符号
-        var mark = "";
-        var i = 0;
-        for (key in postBody) {
-            if (i > 0) {
-                mark = "&";
-            }
-            var value = postBody[key];
-            var paramStr = mark + key + "=" + value;
-            bodyString += paramStr;
-            i++;
-        }
-        return bodyString;
+        return Object.keys(postBody).reduce((total: string, key) => `${total}&${key}=${postBody[key]}`, '').substring(1);
     }
 
     /**
@@ -223,7 +188,7 @@ export default class Request {
      * @param {*} progressFunction 进度返回函数
      * @return 返回Promise
      */
-    static uploadImage(url, path, progressFunction) {
+    static uploadImage(url, path = "", progressFunction) {
         let commonParams = this.params;
         let allCookies = this.cookies;
         const uploadMediaData = new FormData();
@@ -236,7 +201,7 @@ export default class Request {
             var host = "http://yaoser.jd.com";
             var reqUrl = host + url + "?" + commonParams;
             // 上传成功
-            successResponse = xhr => {
+            const successResponse = xhr => {
                 //这个key 就是你上传文件在oss 的地址了，
                 if (xhr._response) {
                     let res = new Function("return " + xhr._response)();
@@ -246,7 +211,7 @@ export default class Request {
                 }
             };
             //上传失败
-            failResponse = xhr => {
+            const failResponse = xhr => {
                 reject(xhr);
             };
 
@@ -274,11 +239,11 @@ export default class Request {
     }
 
     //具体上传的代码
-    static futch(url, opts = {}, onProgress, successResponse, failResponse) {
+    static futch(url, opts = { method: "get", headers: {}, body: {}}, onProgress, successResponse, failResponse) {
         return new Promise((res, rej) => {
             let xhr = new XMLHttpRequest();
-            xhr.open(opts.method || "get", url);
-            for (let k in opts.headers || {}) {
+            xhr.open(opts.method, url);
+            for (let k in opts.headers) {
                 xhr.setRequestHeader(k, opts.headers[k]);
             }
             xhr.onreadystatechange = e => {
