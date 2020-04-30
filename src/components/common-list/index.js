838f91f03e835ef93a5412b69a17e61a9548c8b9
yangtianming3 <yangtianming3@jd.com>
2020-04-30 15:03:48 +0800

    撤销index page修改

--- src/components/common-list/index.js
+++
@@ -1,9 +0,0 @@
-import Taro from "@tarojs/taro";
-
-const currentEnv = Taro.getEnv(); // 获取当前环境平台
-
-if (currentEnv === "RN") {
-    module.exports = require("./common-list-rn").default;
-} else {
-    module.exports = require("./common-list").default;
-}
