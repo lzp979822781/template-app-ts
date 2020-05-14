9b35e2211e1219eabceb1e807c340aa7e3a837b4
yangtianming3 <yangtianming3@jd.com>
2020-05-14 14:38:41 +0800

    添加小程序和rn tab解决方案 添加tabs文件

---
+++ src/pages/List/Tabs/index.tsx
@@ -0,0 +1,72 @@
+import Taro, { Component } from '@tarojs/taro';
+import { ScrollView, View, Text } from '@tarojs/components';
+import { AtTabs, AtTabsPane } from 'taro-ui'
+
+
+class Tabs extends Component {
+    static defaultProps = {
+        current: 0,
+        height: 40,
+        tabList: [
+            { title: 'First Tab' },
+            { title: 'Second Tab' },
+            { title: 'Third Tab' }
+        ],
+        onChange: () => { }
+    }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            current: 0
+        }
+        this.handleClick = this.handleClick.bind(this)
+    }
+
+    handleClick(value) {
+        this.setState({ current: value })
+        const newVal = this.props.tabList[value];
+        this.props.onChange(newVal)
+    }
+
+    renderContent = (data) => {
+        return [1, 2, 3, 4, 5, 6, 7, 8].map(i => {
+            return (
+                <View key={`${data.index}_${i}`} style='padding:40px 0px; margin: 20px; background-color: #ddd; text-align: center;'>
+                    {data.title} - {i}
+                </View>
+            );
+        });
+    }
+
+    renderAtTabsPane = () => {
+        const { tabList } = this.props;
+
+        return tabList.map((item, index) => {
+            return <AtTabsPane current={this.state.current} index={index} key={index.index} >
+                <ScrollView style={{
+                    // 获取设备信息，小程序 h5,需要一个固定高度去支持滚动，注：设置height：100%无效，必须是固定高度
+                    height: `150px`
+                }}
+                    scrollY
+                    scrollWithAnimation
+                >
+                    {this.renderContent(item)}
+                </ScrollView>
+            </AtTabsPane>
+        })
+    }
+
+    render() {
+        const { tabList } = this.props;
+
+        return (
+            <AtTabs current={this.state.current} tabList={tabList} onClick={this.handleClick}>
+                {this.renderAtTabsPane()}
+            </AtTabs>
+        )
+    }
+}
+
+
+export default Tabs;
