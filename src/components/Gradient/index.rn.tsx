26c69d713a2c0d69ddbe359db82081aadb5b70e1
yangtianming3 <yangtianming3@jd.com>
2020-07-03 15:15:11 +0800

    添加渐变传角度

--- src/components/Gradient/index.rn.tsx
+++ src/components/Gradient/index.rn.tsx
@@ -1,28 +1,42 @@
-import Taro, { Component } from '@tarojs/taro';
+import Taro, { Component } from "@tarojs/taro";
 import { LinearGradient } from "expo-linear-gradient";
-import './index.scss';
+import "./index.scss";
 
 type PageOwnprops = {
-    children?: any,
-    colors: Array<string>,
-    style?: object,
-    onClick?: (param) => void
-}
+    children?: any;
+    colors: Array<string>;
+    style?: object;
+    angle?: number;
+    onClick?: (param) => void;
+};
 
 class LinearBadge extends Component<PageOwnprops> {
     constructor(props) {
         super(props);
-        this.state = {  };
+        this.state = {};
     }
 
     render() {
-        const { colors, style, onClick } = this.props;
+        const { colors, style, onClick, angle = 90 } = this.props;
+        const startX = 0,
+            startY = 0;
+        let endX, endY;
+
+        if (angle < 45) {
+            endX = 1;
+            endY = Math.tan((Math.PI / 180) * angle);
+        } else {
+            endX = Math.tan((Math.PI / 180) * (90-angle));
+            endY = 1;
+        }
         return (
-            <LinearGradient 
+            <LinearGradient
                 className='gradient-badge'
                 style={style}
                 colors={colors}
                 onClick={onClick}
+                start={{ x: startX, y: startY }}
+                end={{ x: endX, y: endY }}
             >
                 {this.props.children}
             </LinearGradient>
