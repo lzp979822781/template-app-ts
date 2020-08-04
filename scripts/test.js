e6b315dba830b1db06e327e5b8b25e2768ee69ed
liuzhipeng26 <liuzhipeng26@jd.com>
2020-08-04 15:03:23 +0800

    delete test.js

--- scripts/test.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-const dest = path.join(__dirname, 'demo');
-console.log("dest", dest);
-if(!fs.existsSync(dest)) {
-    fs.mkdirSync(dest);
-}
