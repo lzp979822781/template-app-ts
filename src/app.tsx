32228b18d08db8608fcd1c02fa2ec15dbe956155
yangtianming3 <yangtianming3@jd.com>
2020-08-24 17:01:25 +0800

    删除多余路由和小程序登陆配置插件

--- src/app.tsx
+++ src/app.tsx
@@ -36,55 +36,11 @@ class App extends Component {
         'pages/home/Home/index',
         'pages/BusinessList/index',
         'pages/Index/index',
-        'pages/List/index',
-        'pages/Form/index',
-        'pages/Test/index',
-        'pages/ReferPage/index',
-        'pages/user/User/index',
-        'pages/cart/Cart/index',
-        'pages/home/search/Search/index',
-        'pages/home/search/components/SearchGoodsList/index',
     ],
     window: {
       backgroundTextStyle: 'light',
       navigationBarBackgroundColor: '#fff',
       navigationBarTextStyle: 'black',
-    },
-    /* tabBar: {
-        color: '#989CA5',
-        selectedColor: '#4381E5',
-        list: [
-            {
-                pagePath: "pages/home/Home/index",
-                iconPath: "https://img13.360buyimg.com/imagetools/jfs/t1/135587/24/6938/1857/5f34efd3E554a02e0/96676a809c3443b3.png",
-                selectedIconPath: "./assets/tab-bar/home-active.png",
-                text: "首页"
-            }, 
-            {
-                pagePath: "pages/BusinessList/index",
-                iconPath: "./assets/tab-bar/cate.png",
-                selectedIconPath: "./assets/tab-bar/cate-active.png",
-                text: "合作厂商"
-            }, 
-            {
-                pagePath: "pages/cart/Cart/index",
-                iconPath: "./assets/tab-bar/cart.png",
-                selectedIconPath: "./assets/tab-bar/cart-active.png",
-                text: "购物车"
-            }, 
-            {
-                pagePath: "pages/user/User/index",
-                iconPath: "./assets/tab-bar/user.png",
-                selectedIconPath: "./assets/tab-bar/user-active.png",
-                text: "个人"
-            }
-        ]
-    }, */
-    plugins: {
-        loginPlugin: {
-            "version": "1.1.8",
-            "provider": "wxefe655223916819e"
-        }
     }
 }
 
