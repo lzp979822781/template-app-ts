41bd65bfbc4e191da16793f5af071731b5763fe8
liuzhipeng26 <liuzhipeng26@jd.com>
2020-09-22 20:47:56 +0800

    add start-time select and end time select

--- src/pages/Details/components/DetailPopup/index.tsx
+++ src/pages/Details/components/DetailPopup/index.tsx
@@ -1,6 +1,6 @@
 import Taro, { Component } from "@tarojs/taro";
-import { View, Image } from "@tarojs/components";
-import { PopUp } from "@/components";
+import { View, Image, Text } from "@tarojs/components";
+import { PopUp, Gradient } from "@/components";
 
 import './index.scss';
 
@@ -17,8 +17,20 @@ const rnStyle = {
     height: 344
 };
 
+const rnContainerStyle = {
+    paddingHorizontal: 0
+}
+
 const rnBodyStyle = {
     paddingTop: 0
+};
+
+const resetShadow = {
+    shadowColor: "#000000",
+    shadowOffset: { w: 0, h: 2 },
+    shadowOpacity: 0.04,
+    shadowRadius: 4,
+    elevation: 2
 }
 
 const prefix = 'detail-popup';
@@ -40,12 +52,65 @@ class DetailPopup extends Component<pageOwnProps> {
                 visible={visible}
                 rnStyle={rnStyle}
                 rnBodyStyle={rnBodyStyle}
+                rnContainerStyle={rnContainerStyle}
             >
                 <View className={prefix}>
                     <View className={`${prefix}-header`} >
                         <View className={`${prefix}-header-container`} onClick={onClose}>
                             <Image className={`${prefix}-header-container-img`} src={closeSrc} />
                         </View>
+                    </View>
+                    <View className={`${prefix}-body`}>
+                        <View className={`${prefix}-title`}>
+                            <View className={`${prefix}-text-container`}>
+
+                                <View className={`${prefix}-text-container-container`}>
+
+                                    <View className={`${prefix}-text-container-icon`}>
+                                        <Text className={`${prefix}-text-container-icon-icon`}>始</Text>
+                                    </View>
+                                    <Text className={`${prefix}-text-container-icon-text`}>起始日期</Text>
+                                </View>
+
+                                <View className={`${prefix}-text-container-content`}>
+                                    <Text className={`${prefix}-text-container-text`}>2019.09.28</Text>
+                                </View>
+                            </View>
+                            
+                            <View className={`${prefix}-body-delimiter`}>
+
+                            </View>
+
+                            <View className={`${prefix}-time-conatainer`}>
+                                <View className={`${prefix}-time-conatainer-title-up`}>
+                                    <View className={`${prefix}-time-conatainer-title-up-left`}>
+                                        <Text className={`${prefix}-time-conatainer-title-up-left-text`}>末</Text>
+                                    </View>
+                                    <Text className={`${prefix}-time-conatainer-title-up-right-text`}>结束日期</Text>
+                                </View>
+                                <View className={`${prefix}-time-conatainer-title-down`}>
+                                    <Text className={`${prefix}-time-conatainer-title-down-text`}>2019.09.28</Text>
+                                </View>
+                            </View>
+                        </View>
+
+                        {/* 日期选择器 */}
+                        <View>
+
+                        </View>
+                    </View>
+
+                    <View className={`${prefix}-btn-container`}>
+                        <View className={`${prefix}-btn-reset`} style={resetShadow}>
+                            <Text className={`${prefix}-btn-reset-text`}>重置</Text>
+                        </View>
+                        <Gradient
+                            className={`${prefix}-btn-ok`}
+                            angle={0}
+                            colors={["#F23030", "#FF511D"]}
+                        >
+                            <Text className={`${prefix}-btn-ok-text`}>确定</Text>
+                        </Gradient>
                     </View>
                 </View>
             </PopUp>
